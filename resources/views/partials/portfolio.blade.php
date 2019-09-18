6f6a97c5331fca323f388345fe1c24e5e08c52ab
Roman <romanm@paulbunyan.net>
2019-09-18 15:29:24 -0500

    made portfolio into carousel

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -6,61 +6,59 @@
                 <h3 class="section-subheading text-muted">Some of our past jobs</h3>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4 col-sm-12 mb-5">
-                <div class="row h-100">
-                    <div class="col-12 portfolio-item mb-5">
-                        <img class="img-fluid" src="img/past-projects/project1/before.jpg" alt="">
-                        <div class="portfolio-caption">
-                            <h4>Before</h4>
-                            <p class="text-muted">7201 NE 8th Ave., Portland, OR</p>
-                        </div>
+        <div id="carouselExampleIndicators" class="carousel slide row" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" style="max-height: 600px; min-height: 370px;" src="img/past-projects/project1/before.jpg" alt="">
+                    <div class="carousel-caption">
+                        <h4>Before</h4>
+                        <p>7201 NE 8th Ave., Portland, OR</p>
                     </div>
-                    <div class="col-12 portfolio-item align-self-end">
-                        <img class="img-fluid" src="img/past-projects/project1/after.jpg" alt="">
-                        <div class="portfolio-caption">
-                            <h4>After</h4>
-                            <p class="text-muted">7201 NE 8th Ave., Portland, OR</p>
-                        </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" style="max-height: 600px; min-height: 370px;" src="img/past-projects/project1/after.jpg" alt="">
+                    <div class="carousel-caption">
+                        <h4>After</h4>
+                        <p >7201 NE 8th Ave., Portland, OR</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" style="max-height: 600px; min-height: 370px;" src="img/past-projects/project2/before.png" alt="">
+                    <div class="carousel-caption">
+                        <h4>Before</h4>
+                        <p >544 N 10th St. , Saint Helens, OR</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" style="max-height: 600px; min-height: 370px;" src="img/past-projects/project2/after.jpg" alt="">
+                    <div class="carousel-caption">
+                        <h4>After</h4>
+                        <p >544 N 10th St. , Saint Helens, OR</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" style="max-height: 600px; min-height: 370px;" src="img/past-projects/project3/before.jpg" alt="">
+                    <div class="carousel-caption">
+                        <h4>Before</h4>
+                        <p >720 McLoughlin BLVD, Oregon City, OR</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" style="max-height: 600px; min-height: 370px;" src="img/past-projects/project3/after.jpg" alt="">
+                    <div class="carousel-caption">
+                        <h4>After</h4>
+                        <p >720 McLoughlin BLVD, Oregon City, OR</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-12 mb-5">
-                <div class="row h-100">
-                    <div class="col-12 portfolio-item mb-5">
-                        <img class="img-fluid" src="img/past-projects/project2/before.png" alt="">
-                        <div class="portfolio-caption">
-                            <h4>Before</h4>
-                            <p class="text-muted">544 N 10th St. , Saint Helens, OR</p>
-                        </div>
-                    </div>
-                    <div class="col-12 portfolio-item align-self-end">
-                        <img class="img-fluid" src="img/past-projects/project2/after.jpg" alt="">
-                        <div class="portfolio-caption">
-                            <h4>After</h4>
-                            <p class="text-muted">544 N 10th St. , Saint Helens, OR</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-12 mb-5">
-                <div class="row h-100">
-                    <div class="col-12 portfolio-item mb-5">
-                        <img class="img-fluid" src="img/past-projects/project3/before.jpg" alt="">
-                        <div class="portfolio-caption">
-                            <h4>Before</h4>
-                            <p class="text-muted">720 McLoughlin BLVD, Oregon City, OR</p>
-                        </div>
-                    </div>
-                    <div class="col-12 portfolio-item align-self-end">
-                        <img class="img-fluid" src="img/past-projects/project3/after.jpg" alt="">
-                        <div class="portfolio-caption">
-                            <h4>After</h4>
-                            <p class="text-muted">720 McLoughlin BLVD, Oregon City, OR</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 </section>
